aa7b085b4f2fe7bfafb3b08b65ce0aa2aae7e999
Uesli Keta <44416957+FestiveChicken@users.noreply.github.com>
2023-12-19 15:14:28 -0500

    Need to add webpack

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const submitButton = document.getElementById('submit').addEventListener('click',
 //Gets temperature for the next five days
 const getTemperature = async(locationName) => {
     return new Promise((resolve, reject) => {
+
     const temperatureArray = [];
+
     fetch('https://api.weatherapi.com/v1/forecast.json?key=302e8a0e7fec43a1b5a181824231312&q=' + locationName + '&days=5', { mode: 'cors' })
     .then(function (response) {
         return response.json();
     })
     .then(function (response) {
-        for (i = 0; i < 5; i++) {
+        for (let i = 0; i < 5; i++) {
             let temperature = response.forecast.forecastday[i].day.avgtemp_c;
             temperatureArray.push(temperature);
         }
@@ -27,42 +29,79 @@ const getTemperature = async(locationName) => {
 };
 
 //Gets conditions for the next five days
-const getCondition = (locationName) => {
-    const conditonsArray = [];
-
-    fetch('https://api.weatherapi.com/v1/forecast.json?key=302e8a0e7fec43a1b5a181824231312&q=' + locationName + '&days=5', { mode: 'cors' })
-    .then(function (response) {
-        return response.json();
+const getCondition = async (locationName) => {
+    return new Promise((resolve, reject) => {
+        const conditionsArray = [];
+        fetch('https://api.weatherapi.com/v1/forecast.json?key=302e8a0e7fec43a1b5a181824231312&q=' + locationName + '&days=5', { mode: 'cors' })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            for (i = 0; i < 5; i++) {
+                let condition = response.forecast.forecastday[i].day.condition.text;
+                conditionsArray.push(condition)
+            }
+            resolve(conditionsArray)
+        })
+        .catch(function (error) {
+            // Handle error by displaying a message
+            console.log('Error fetching data:', error);
+        });
     })
-    .then(function (response) {
-        for (i = 0; i < 5; i++) {
-            let condition = response.forecast.forecastday[i].day.condition.text;
-            conditonsArray.push(condition)
-            console.log(conditonsArray)
-        }
-    })
-    .catch(function (error) {
-        // Handle error by displaying a message
-        console.log('Error fetching data:', error);
-    });
-    return conditonsArray
 };
 
 //Sets the picture for each day
-const setWeatherPictures = (locationName) => {
-    const dayOneImage = document.getElementById('dayOneImage');
-    const dayTwoImage = document.getElementById('dayTwoImage');
-    const dayThreeImage = document.getElementById('dayThreeImage');
-    const dayFourImage = document.getElementById('dayFourImage');
-    const dayFiveImage = document.getElementById('dayFiveImage');
+const setWeatherPictures = async (locationName) => {
+    try {
+        const conditionsArray = await getCondition(locationName)
+        //const day1Image = document.getElementById('day1Image');
+        //const day2Image = document.getElementById('day2Image');
+        //const day3Image = document.getElementById('day3Image');
+        //const day4Image = document.getElementById('day4Image');
+        //const day5Image = document.getElementById('day5Image');
+        //console.log(conditionsArray)
+        for (let i = 0; i < conditionsArray.length; i++) {
+            const iconNumber = getIconNumber(conditionsArray[i])
+            const imageId = `day${i+1}Image`;
+            const imagePath = `/images${iconNumber}.png`
+            console.log('im doing the thing')
+            document.getElementById(imageId).src = imagePath
+        }
+    }
+    catch (error) {
+    }
+}
+
+//fetches weather conditions
+const fetchWeatherConditions = async () => {
+    try {
+        const response = await fetch('/weather_conditions.json');
+        const weatherConditions = await response.json()
+        return weatherConditions
+    }
+    catch (error) {
+        console.log('error getting weather JSON')
+    }
+}
+
+//gets icon number from weather_conditions.json
+const getIconNumber = async (condition) => {
+    const icon = [];
+    if (typeof weatherConditions !== 'undefinded') {
+        const weatherConditions = await fetchWeatherConditions();
+    }
+    const matchingCondition = weatherConditions.find(item => item.day === day)
+    if (matchingCondition) {
+        icon.push(matchingCondition.icon);
+    }
+    else console.log('getIconNumber')
+    return icon
 }
 
 //Sets the description for each day
     const  setWeatherDescription = async (locationName) => {
         try {
-            const temperature =  await getTemperature(locationName)
-            console.log(temperature)
-            
+            const temperature =  await getTemperature(locationName)            
             const dayOneDescription = document.getElementById('dayOneDescription');
             const dayTwoDescription = document.getElementById('dayTwoDescription');
             const dayThreeDescription = document.getElementById('dayThreeDescription');
@@ -93,7 +132,6 @@ const setWeatherPictures = (locationName) => {
             };
         }
         catch (error) {
-            console.log('get fucked')
         }
     };
 
